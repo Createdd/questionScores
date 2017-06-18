8cf74770fe12c77b5d5f22541f39a55e687a96f5
Daniel Deutsch <deudan1010@gmail.com>
2017-06-18 16:29:12 +0200

    add reducer

---
+++ src/reducers/question.js
@@ -0,0 +1,43 @@
+import * as QuestionActionTypes from '../actiontypes/question';
+
+const initialState = [
+  {
+    name: 'Do you like AI?',
+    score: 31,
+  },
+  {
+    name: 'Do you like Engineering?',
+    score: 20,
+  },
+  {
+    name: 'How many Redux Apps?',
+    score: 50,
+  },
+];
+
+export default function Player(state = initialState, action) {
+  switch (action.type) {
+    case QuestionActionTypes.ADD_PLAYER:
+      return [
+        ...state,
+        {
+          name: action.name,
+          score: 0,
+        },
+      ];
+    case QuestionActionTypes.REMOVE_QUESTION:
+      return [...state.slice(0, action.index), ...state.sclice(action.index + 1)];
+    case QuestionActionTypes.UPDATE_QUESTION_SCORE:
+      return state.map((question, index) => {
+        if (index === action.index) {
+          return {
+            ...question,
+            score: question.score + question.score,
+          };
+        }
+        return question;
+      });
+    default:
+      return state;
+  }
+}
